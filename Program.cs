3decc051d85146295ae95b508a7f53011eaad12a
Martin Honnen <martin.honnen@gmx.de>
2024-05-06 15:54:33 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Python.Runtime;
+
+PythonEngine.Initialize();
+
+using var _ = Py.GIL();
+
+dynamic xmlschema = Py.Import("xmlschema");
+
+using dynamic sampleSchema = xmlschema.XMLSchema11("assert-example1.xsd");
+
+Console.WriteLine(sampleSchema.is_valid("names-valid.xml"));
+
+Console.WriteLine(sampleSchema.is_valid("names-invalid.xml"));
+
+
+try
+{
+    sampleSchema.validate("names-invalid.xml");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
